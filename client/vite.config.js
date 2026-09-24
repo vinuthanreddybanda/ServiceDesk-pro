fb859f20daa77a80836a3e22d4b1fb6ee37edd74
vinuthanreddybanda <vinuthanreddybanda@gmail.com>
2026-09-24 14:04:00 +0530

    Add Tailwind CSS\

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),tailwindcss()],
   server: {
     port: 5173,
     proxy: {
